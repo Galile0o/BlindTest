888e33fc0c6124bcd58d63c681bcb6966036196a
Galile0o <72972521+Galile0o@users.noreply.github.com>
2023-12-09 19:12:15 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -168,13 +168,13 @@ function lancerEcoute(titre) {
 
     function passerPisteSuivante() {
         indexPisteActuelle = (indexPisteActuelle + 1) % pistes.length;
-        console.log(indexPisteActuelle);
+        console.log("test : " + indexPisteActuelle);
         jouerPisteActuelle();
     }
 
     function reculerPistePrecedente() {
         indexPisteActuelle = (indexPisteActuelle - 1 + pistes.length) % pistes.length;
-        console.log(indexPisteActuelle);
+        console.log("test : " + indexPisteActuelle);
         jouerPisteActuelle();
     }
 
