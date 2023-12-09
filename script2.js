2fab1ca318c023b6c6e8f319565c07aad1dc0184
Galile0o <72972521+Galile0o@users.noreply.github.com>
2023-12-09 19:19:22 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -167,15 +167,37 @@ function lancerEcoute(titre) {
     }
 
     function passerPisteSuivante() {
+        // Vérifier si une opération de passage est déjà en cours
+        if (enCoursDePassage) {
+            return;
+        }
+
+        // Désactiver la possibilité de passer pendant cette opération
+        enCoursDePassage = true;
+
         indexPisteActuelle = (indexPisteActuelle + 1) % pistes.length;
-        console.log("test : " + indexPisteActuelle);
         jouerPisteActuelle();
+
+        // Activer à nouveau la possibilité de passer après un court délai
+        setTimeout(() => {
+            enCoursDePassage = false;
+        }, 1000); // Ajoutez un délai d'1 seconde (ajustable si nécessaire)
     }
 
     function reculerPistePrecedente() {
+         if (enCoursDePassage) {
+            return;
+        }
+
+        // Désactiver la possibilité de passer pendant cette opération
+        enCoursDePassage = true;
+        
         indexPisteActuelle = (indexPisteActuelle - 1 + pistes.length) % pistes.length;
-        console.log("test : " + indexPisteActuelle);
         jouerPisteActuelle();
+        
+        setTimeout(() => {
+            enCoursDePassage = false;
+        }, 1000); // Ajoutez un délai d'1 seconde (ajustable si nécessaire)
     }
 
     function jouerPisteActuelle() {
@@ -194,6 +216,8 @@ function lancerEcoute(titre) {
             lecteurAudio.pause();
         }
     }
+
+    let enCoursDePassage = false; // Nouvelle variable pour éviter les appels multiples
 
     // Retirer tous les écouteurs d'événements
     lecteurAudio.removeEventListener('loadedmetadata', mettreAJourBarreDeTemps);
@@ -223,6 +247,7 @@ function lancerEcoute(titre) {
 
     lecteurAudio.play();
 }
+
 
 
 
